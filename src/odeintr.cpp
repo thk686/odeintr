e0f90a5c455cff5b66fbec7d2f3834458f0bd2a3
Tim Keitt <tkeitt@gmail.com>
2015-04-23 16:40:23 -0500

    Reserve storage, const and adaptive observer calls

--- src/odeintr.cpp
+++ src/odeintr.cpp
@@ -4,6 +4,9 @@
 static std::vector<List> rec_x;
 static std::vector<double> rec_t;
 
+using stepper_type = odeint::runge_kutta_dopri5<state_type>;
+auto stepper = odeint::make_dense_output(1e-6, 1e-6, stepper_type());
+
 struct Sys
 {
   Sys(Function f) : derivs(f) {}
@@ -32,18 +35,29 @@ struct Obs
 };
 
 // [[Rcpp::export]]
-List integrate_sys_(Function derivs, Function recfun, state_type init,
-                    double duration, double step_size = 1.0,
-                    double start = 0.0)
+List integrate_sys_const(Function derivs, Function recfun, state_type init,
+                         double duration, double step_size = 1.0,
+                         double start = 0.0)
 {
-  rec_x.resize(0);
-  rec_t.resize(0);
-  Sys s(derivs);
-  Obs o(recfun);
-  odeint::integrate(s, init, start, start + duration, step_size, o);
-  List out;
-  out("t") = rec_t;
-  out("x") = rec_x;
+  rec_x.resize(0); rec_t.resize(0);
+  rec_x.reserve(duration / step_size);
+  rec_t.reserve(duration / step_size);
+  Sys s(derivs); Obs o(recfun);
+  odeint::integrate_const(stepper, s, init, start, start + duration, step_size, o);
+  List out; out("t") = rec_t; out("x") = rec_x;
   return out;
 }
 
+// [[Rcpp::export]]
+List integrate_sys_adapt(Function derivs, Function recfun, state_type init,
+                         double duration, double step_size = 1.0,
+                         double start = 0.0)
+{
+  rec_x.resize(0); rec_t.resize(0);
+  rec_x.reserve(duration / step_size);
+  rec_t.reserve(duration / step_size);
+  Sys s(derivs); Obs o(recfun);
+  odeint::integrate_adaptive(stepper, s, init, start, start + duration, step_size, o);
+  List out; out("t") = rec_t; out("x") = rec_x;
+  return out;
+}
