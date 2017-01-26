15049414ee4c9a3eb009d13af04e75eb2898d99e
Tim Keitt <tkeitt@gmail.com>
2017-01-26 12:03:08 -0600

    Can't call sourceCpp in testthat

--- tests/testthat/test_odeintr.R
+++ tests/testthat/test_odeintr.R
@@ -8,10 +8,18 @@ test_that("integrate_sys works", {
   expect_equal(res[1, 2], 0.01)
 })
 
-test_that("compile_sys works", {
-  compile_sys("logistic", "dxdt = x * (1 - x)")
-  res = logistic(0.01, 40)
-  expect_is(res, "data.frame")
-  expect_equal(dim(res), c(41, 2))
-  expect_equal(res[1, 2], 0.01)
-})
+# test_that("compile_sys works", {
+#   compile_sys("logistic", "dxdt = x * (1 - x)")
+#   res = logistic(0.01, 40)
+#   expect_is(res, "data.frame")
+#   expect_equal(dim(res), c(41, 2))
+#   expect_equal(res[1, 2], 0.01)
+# })
+# 
+# test_that("compile_implicit works", {
+#   compile_implicit("logi", "dxdt = x * (1 - x)")
+#   res = logi(0.01, 40)
+#   expect_is(res, "data.frame")
+#   expect_equal(dim(res), c(41, 2))
+#   expect_equal(res[1, 2], 0.01)
+# })
