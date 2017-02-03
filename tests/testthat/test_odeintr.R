da558b21c6594d26ef746ad573ef40b3a3dc91f0
Tim Keitt <tkeitt@gmail.com>
2017-02-03 09:05:06 -0600

    fixed relative paths

--- tests/testthat/test_odeintr.R
+++ tests/testthat/test_odeintr.R
@@ -1,6 +1,11 @@
 library(odeintr)
 context("Solvers")
 
+wd = getwd()
+wd = sub(".tests.testthat$", "", wd)
+ipath = file.path(wd, "inst", "include")
+Sys.setenv(PKG_CXXFLAGS = paste0("-I", ipath))
+
 test_that("integrate_sys works", {
   res = integrate_sys(function(x, t) x * (1 - x), 0.01, 40)
   expect_is(res, "data.frame")
@@ -8,18 +13,19 @@ test_that("integrate_sys works", {
   expect_equal(res[1, 2], 0.01)
 })
 
-# test_that("compile_sys works", {
-#   compile_sys("logistic", "dxdt = x * (1 - x)")
-#   res = logistic(0.01, 40)
-#   expect_is(res, "data.frame")
-#   expect_equal(dim(res), c(41, 2))
-#   expect_equal(res[1, 2], 0.01)
-# })
-# 
-# test_that("compile_implicit works", {
-#   compile_implicit("logi", "dxdt = x * (1 - x)")
-#   res = logi(0.01, 40)
-#   expect_is(res, "data.frame")
-#   expect_equal(dim(res), c(41, 2))
-#   expect_equal(res[1, 2], 0.01)
-# })
+test_that("compile_sys works", {
+  compile_sys("logistic", "dxdt = x * (1 - x)")
+  res = logistic(0.01, 40)
+  expect_is(res, "data.frame")
+  expect_equal(dim(res), c(41, 2))
+  expect_equal(res[1, 2], 0.01)
+})
+
+test_that("compile_implicit works", {
+  compile_implicit("logi", "dxdt[0] = x[0] * (1 - x[0])")
+  res = logi(0.01, 40)
+  expect_is(res, "data.frame")
+  expect_equal(dim(res), c(41, 2))
+  expect_equal(res[1, 2], 0.01)
+})
+
