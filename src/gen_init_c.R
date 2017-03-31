dbf629b47916559cca2683d2a21ac156e4fd550b
Tim Keitt <tkeitt@gmal.com>
2017-03-30 21:44:41 -0500

    automate function registration

---
+++ src/gen_init_c.R
@@ -0,0 +1,28 @@
+contents = readLines("src/RcppExports.cpp")
+protos = grep("^RcppExport", contents, value = TRUE)
+funcs = sub("^RcppExport SEXP (.*)\\(.*", "\\1", protos)
+nargs = sapply(gregexpr("\\<SEXP\\>", protos), length) - 1
+gen_call_method = function(fname, nargs)
+{
+  if (is.na(fname) || is.na(nargs))
+    return("\t{NULL, NULL, 0}};")
+  return(paste0('\t{"', fname, '", (DL_FUNC) &',
+                fname, ', ', nargs, '}'))
+}
+funcs = c(funcs, NA); nargs = c(nargs, NA)
+cms = lapply(1:length(funcs), function(i) gen_call_method(funcs[i], nargs[i]))
+  
+cat("#include <R.h>\n", file = "src/init.c")
+cat("#include <Rinternals.h>\n", file = "src/init.c", append = TRUE)
+cat("#include <R_ext/Rdynload.h>\n\n", file = "src/init.c", append = TRUE)
+
+protos = sub(" \\{$", ";", sub("RcppExport", "extern", protos));
+protos = paste0(protos, collapse = "\n")
+cat(protos, file = "src/init.c", append = TRUE)
+
+cat("\n\nR_CallMethodDef callMethods[]  = {\n", file = "src/init.c", append = TRUE)
+cat(paste(cms, collapse = ",\n"), file = "src/init.c", append = TRUE)
+cat("\n\nvoid\nR_init_myLib(DllInfo *info)\n{\n", file = "src/init.c", append = TRUE)
+cat("\tR_registerRoutines(info, NULL, callMethods, NULL, NULL);\n", file = "src/init.c", append = TRUE)
+cat("\tR_useDynamicSymbols(info, FALSE);\n}", file = "src/init.c", append = TRUE)
+
--- src/init.c
+++ src/init.c
@@ -2,12 +2,17 @@
 #include <Rinternals.h>
 #include <R_ext/Rdynload.h>
 
-R_CallMethodDef callMethods[]  = {{NULL, NULL, 0}};
-R_CMethodDef cMethods[] = {{NULL, NULL, 0}};
+extern SEXP odeintr_integrate_sys_const(SEXP derivsSEXP, SEXP recfunSEXP, SEXP initSEXP, SEXP durationSEXP, SEXP step_sizeSEXP, SEXP startSEXP, SEXP atolSEXP, SEXP rtolSEXP);
+extern SEXP odeintr_integrate_sys_adapt(SEXP derivsSEXP, SEXP recfunSEXP, SEXP initSEXP, SEXP durationSEXP, SEXP step_sizeSEXP, SEXP startSEXP, SEXP atolSEXP, SEXP rtolSEXP);
+
+R_CallMethodDef callMethods[]  = {
+	{"odeintr_integrate_sys_const", (DL_FUNC) &odeintr_integrate_sys_const, 8},
+	{"odeintr_integrate_sys_adapt", (DL_FUNC) &odeintr_integrate_sys_adapt, 8},
+	{NULL, NULL, 0}};
 
 void
 R_init_myLib(DllInfo *info)
 {
-  R_registerRoutines(info, cMethods, callMethods, NULL, NULL);
-  R_useDynamicSymbols(info, FALSE);
+	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+	R_useDynamicSymbols(info, FALSE);
 }
